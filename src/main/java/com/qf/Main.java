52eebaa4233a57f17b12b9c634bfdaafe721a21f
wdx9652 <1761814150@qq.com>
2019-04-07 15:39:17 +0800

    ...

--- src/main/java/com/qf/Main.java
+++ src/main/java/com/qf/Main.java
@@ -3,5 +3,6 @@ package com.qf;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Word???");
+        System.out.println("今天你要提交的项目，赶紧的");
     }
 }
